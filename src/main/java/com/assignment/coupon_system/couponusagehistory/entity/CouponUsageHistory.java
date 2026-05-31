43cf6bbb5a5c02e40af37e94afda9f1fbfbe7aff
jin <wlsrl723@gmail.com>
2026-06-01 01:48:49 +0900

    CouponUsageHistory entity 구현

--- src/main/java/com/assignment/coupon_system/couponusagehistory/entity/CouponUsageHistory.java
+++ src/main/java/com/assignment/coupon_system/couponusagehistory/entity/CouponUsageHistory.java
@@ -1,4 +1,45 @@
 package com.assignment.coupon_system.couponusagehistory.entity;
 
+import com.assignment.coupon_system.issuedcoupon.entity.IssuedCoupon;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
 public class CouponUsageHistory {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "issued_coupon_id")
+    private IssuedCoupon issuedCoupon;
+
+//    private Long userId;
+
+    private Long orderId;
+
+    private Integer discountAmount;
+
+    private LocalDateTime usedAt;
+
+    public static CouponUsageHistory save(
+            IssuedCoupon issuedCoupon,
+            Long orderId,
+            Integer discountAmount
+    ) {
+        CouponUsageHistory couponUsageHistory = new CouponUsageHistory();
+        couponUsageHistory.issuedCoupon = issuedCoupon;
+        couponUsageHistory.orderId = orderId;
+        couponUsageHistory.discountAmount = discountAmount;
+        couponUsageHistory.usedAt = LocalDateTime.now();
+
+        return couponUsageHistory;
+    }
+
 }
