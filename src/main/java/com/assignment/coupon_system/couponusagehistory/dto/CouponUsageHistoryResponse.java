33f76a11fae5c130a3ba09661e33830f877f998f
jin <wlsrl723@gmail.com>
2026-06-01 02:33:51 +0900

    CouponUsageHistoryResponse 구현

--- src/main/java/com/assignment/coupon_system/couponusagehistory/dto/CouponUsageHistoryResponse.java
+++ src/main/java/com/assignment/coupon_system/couponusagehistory/dto/CouponUsageHistoryResponse.java
@@ -1,4 +1,29 @@
 package com.assignment.coupon_system.couponusagehistory.dto;
 
+import com.assignment.coupon_system.couponusagehistory.entity.CouponUsageHistory;
+import lombok.Builder;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Builder
 public class CouponUsageHistoryResponse {
+
+    private Long id;
+    private Long issuedCouponId;
+    private Long orderId;
+    private Integer discountAmount;
+    private LocalDateTime usedAt;
+
+
+    public static CouponUsageHistoryResponse from(CouponUsageHistory couponUsageHistory) {
+        return CouponUsageHistoryResponse.builder()
+                .id(couponUsageHistory.getId())
+                .issuedCouponId(couponUsageHistory.getIssuedCoupon().getId())
+                .orderId(couponUsageHistory.getOrderId())
+                .discountAmount(couponUsageHistory.getDiscountAmount())
+                .usedAt(couponUsageHistory.getUsedAt())
+                .build();
+    }
 }
