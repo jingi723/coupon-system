5a6a8612e4b4ca4543e81aa54cd7fab0e255f543
jin <wlsrl723@gmail.com>
2026-06-01 02:33:41 +0900

    CouponUsageHistoryRequest 구현

--- src/main/java/com/assignment/coupon_system/couponusagehistory/dto/CouponUsageHistoryRequest.java
+++ src/main/java/com/assignment/coupon_system/couponusagehistory/dto/CouponUsageHistoryRequest.java
@@ -1,4 +1,21 @@
 package com.assignment.coupon_system.couponusagehistory.dto;
 
+import com.assignment.coupon_system.issuedcoupon.entity.IssuedCoupon;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class CouponUsageHistoryRequest {
+
+    private IssuedCoupon issuedCoupon;
+    private Long orderId;
+
 }
