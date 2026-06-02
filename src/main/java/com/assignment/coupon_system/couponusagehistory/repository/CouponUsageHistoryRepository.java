8a50c698e296e2c0d7b40f301aa74fd0fa83f4e5
jin <wlsrl723@gmail.com>
2026-06-02 12:36:17 +0900

    CouponUsageHistoryRepository 수정

--- src/main/java/com/assignment/coupon_system/couponusagehistory/repository/CouponUsageHistoryRepository.java
+++ src/main/java/com/assignment/coupon_system/couponusagehistory/repository/CouponUsageHistoryRepository.java
@@ -3,5 +3,8 @@ package com.assignment.coupon_system.couponusagehistory.repository;
 import com.assignment.coupon_system.couponusagehistory.entity.CouponUsageHistory;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CouponUsageHistoryRepository extends JpaRepository<CouponUsageHistory, Long> {
+    List<CouponUsageHistory> findByIssuedCouponUserId(Long userId);
 }
