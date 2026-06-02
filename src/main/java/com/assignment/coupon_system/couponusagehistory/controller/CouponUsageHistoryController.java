a03f5ad3697d7f9c416b3687f8740172e394d2f8
jin <wlsrl723@gmail.com>
2026-06-02 12:38:07 +0900

    CouponUsageHistoryController 구현

--- src/main/java/com/assignment/coupon_system/couponusagehistory/controller/CouponUsageHistoryController.java
+++ src/main/java/com/assignment/coupon_system/couponusagehistory/controller/CouponUsageHistoryController.java
@@ -1,4 +1,32 @@
 package com.assignment.coupon_system.couponusagehistory.controller;
 
+import com.assignment.coupon_system.common.response.ApiResponse;
+import com.assignment.coupon_system.couponusagehistory.dto.CouponUsageHistoryRequest;
+import com.assignment.coupon_system.couponusagehistory.dto.CouponUsageHistoryResponse;
+import com.assignment.coupon_system.couponusagehistory.service.CouponUsageHistoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
 public class CouponUsageHistoryController {
+
+    private final CouponUsageHistoryService couponUsageHistoryService;
+
+    @PostMapping("/api/coupons/usage/{issuedCouponId}")
+    public ApiResponse<CouponUsageHistoryResponse> useCoupon(
+            @PathVariable Long issuedCouponId,
+            @RequestBody CouponUsageHistoryRequest couponUsageHistoryRequest
+    ) {
+        return ApiResponse.success(couponUsageHistoryService.useCoupon(issuedCouponId, couponUsageHistoryRequest));
+    }
+
+    @GetMapping("/api/coupons/usage/history/{userId}/")
+    public ApiResponse<List<CouponUsageHistoryResponse>> getCouponUsageHistory(
+            @PathVariable Long userId
+    )  {
+        return ApiResponse.success(couponUsageHistoryService.getUsageHistory(userId));
+    }
 }
